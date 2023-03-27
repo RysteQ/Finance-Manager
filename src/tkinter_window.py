1071f66f154a62c30518e759e820b77414788eff
RysteQ <34135554+RysteQ@users.noreply.github.com>
2023-03-27 09:45:29 +0300

    Update tkinter_window.py
    I need to fix this annoying bug that has the textboxes having a empty space between them because the buttons above them occupy more space

--- src/tkinter_window.py
+++ src/tkinter_window.py
@@ -30,9 +30,38 @@ def initGUI():
         command = DisplayGraph
     )
 
+    week_one_label = Tk.Label(main_window, text = "Week 1")
+    week_two_label = Tk.Label(main_window, text = "Week 2")
+    week_three_label = Tk.Label(main_window, text = "Week 3")
+    week_four_label = Tk.Label(main_window, text = "Week 4")
+    week_five_label = Tk.Label(main_window, text = "Week 5")
+
+    all_weeks = [
+        [],
+        [],
+        [],
+        [],
+        []
+    ]
+
+    for i in range(5):
+        for j in range(7):
+            textbox_to_append = Tk.Text(main_window, height = 1, width = 7)
+            all_weeks[i].append(textbox_to_append)
+
     open_csv_file_button.grid(column = 0, row = 0)
     save_csv_file_button.grid(column = 1, row = 0)
-    display_csv_graph_button.grid(column = 0, row = 1, columnspan = 2)
+    display_csv_graph_button.grid(column = 2, row = 0)
+
+    week_one_label.grid(column = 0, row = 1)
+    week_two_label.grid(column = 0, row = 2)
+    week_three_label.grid(column = 0, row = 3)
+    week_four_label.grid(column = 0, row = 4)
+    week_five_label.grid(column = 0, row = 5)
+
+    for i in range(5):
+        for j in range(7):
+            all_weeks[i][j].grid(column = j + 1, row = i + 1)
 
     return main_window
 
